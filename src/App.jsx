e86ed5a5a843b6555965a26de62b473b266c02ee
usfsaid <youseftaher2100@gmail.com>
2026-09-13 14:50:41 +0300

    s2: 1-update name image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { motion } from "framer-motion";
+import images from "./images";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+
 const App = () => {
-  return <div></div>;
+  const [width, setWidte] = useState();
+  const carouselRef = useRef();
+  useEffect(() => {
+    console.log(carouselRef.current.scrollWidth);
+    console.log(carouselRef.current.offsetWidth);
+    setWidte(carouselRef.current.scrollWidth - carouselRef.current.offsetWidth);
+  }, []);
+  return (
+    <div>
+      <motion.div
+        className={"carousel"}
+        ref={carouselRef}
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className={"inner-carousel"}
+        >
+          {images.map((image) => (
+            <motion.div className="item" key={image}>
+              <img src={image} alt="carousel-img" />
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default App;
